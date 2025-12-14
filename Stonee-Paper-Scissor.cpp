8a9ac7f3aad554c1e0bb4f8e2e76ecbfceff10ba
Ahmed alamin <narout2015@gmail.com>
2025-12-14 19:17:57 +0300

    add  User choic & and Pc choic

--- Stonee-Paper-Scissor.cpp
+++ Stonee-Paper-Scissor.cpp
@@ -11,44 +11,81 @@ int HowmanyRounds(string Message )
     return Number;
 }
 
-Mychose choice(int choice )
+Mychose User_Choice( )
 {
-     choice = HowmanyRounds("your choice : [1]:stone , [2]:paper, [3]: Scissor ? ");
+    int choice = 0;
+   cout<<"your choice : [1]:stone , [2]:paper, [3]: Scissor ? \n";
+   cin >> choice;
 
 
     if (choice == 1)
-        return Mychose::Stone;
+       return Stone;
 
     if (choice == 2)
-        return Mychose::Paper;
+        return Paper;
 
     if (choice == 3)
-        return Mychose::Scissor;
+        return  Scissor;
 }
 
-int RandomChoicePc(int From, int To)
+Mychose PC_Choice()
 {
-    int randNum = rand() % (To - From + 1) + From;
-    return randNum; 
+    int pcchoice = rand() % 3 + 1;
+    return static_cast<Mychose>(pcchoice);
 }
-
-int  RandomPCChoice()
+void Determine_Winner(Mychose user, Mychose pc)
 {
-    return RandomChoicePc(1, 3);
+	int playerWinCount = 0;
+	int pcWinCount = 0;
+	int drawCount = 0;
+    if (user == pc)
+    {
+        cout << "No winner!" << endl;
+        drawCount++;
+    }
+    else if (
+        (user == Stone && pc == Scissor) ||
+        (user == Paper && pc == Stone) ||
+        (user == Scissor && pc == Paper))
+    {
+        cout << "[Player win]" << endl;
+        playerWinCount++;
+    }
+    else
+    {
+        cout << "[PC wins]" << endl;
+		pcWinCount++;
+    }
 }
 
 
 
  
-int sartgame()
+int Startgame()
 {
     int Number = HowmanyRounds("who many Rounds 1 to 10  ? ");
-    for (int i = 1; i < Number; i++)
+    for (int i = 1; i <=Number; i++)
     {
-        cout << "Round [" << i << "] begins :" << endl;
-        
+        cout << "Round [" << i << "] begins :\n" << endl;
+        Mychose user = User_Choice();
+        Mychose Pc = PC_Choice();
+		cout << "             Round                           "<<endl;
+        cout << "----------     [" << i << "]--------------\n" << endl;
+		cout << "Player   choice is : " << user << endl;
+		cout << "Computer choice is : " << Pc << endl;
+        cout << "Round Winner       : ";
+            Determine_Winner(user, Pc);
+			cout << "------------------------------\n" << endl;
+            cout << "-------------------------------\n" << endl;
+            cout << "         Game Over          \n" << endl;
+            cout << "-------------------------------\n" << endl;
+            cout << "----------Game Result----------\n" << endl;
+            cout << "Game Rounds   " << Number;
+            cout << "Player won times "<< Determine_Winner()
+
 
     }
+	return 0;
 }
 
 
@@ -56,6 +93,8 @@ int main()
 {
     srand((unsigned)time(NULL));
 
+	Startgame();
+    
 
 }
 
